c0242c1c67fba70185751d3c6358c80b8b39686f
Luciano Zancan Mazzutti <lmazzutti@nelogica.com.br>
2020-04-05 16:06:00 -0300

    Project Setup and Navigation

--- app/src/main/java/com/example/memorynotes/presentation/NoteFragment.kt
+++ app/src/main/java/com/example/memorynotes/presentation/NoteFragment.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 
 import com.example.memorynotes.R
+import kotlinx.android.synthetic.main.fragment_note.*
 
 class NoteFragment : Fragment() {
 
@@ -16,6 +18,13 @@ class NoteFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_note, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        fab_check.setOnClickListener{
+            Navigation.findNavController(it).popBackStack()
+        }
     }
 
 }
