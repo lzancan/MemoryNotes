aef66a80dd84b51c0122b8e560d9141b90954302
Luciano Zancan Mazzutti <lmazzutti@nelogica.com.br>
2020-04-05 15:58:38 -0300

    Initial commit

---
+++ core/src/main/java/com/example/core/repository/NoteRepository.kt
@@ -0,0 +1,14 @@
+package com.example.core.repository
+
+import com.example.core.data.Note
+
+class NoteRepository(private val noteDataSource: NoteDataSource) {
+
+    suspend fun addNote(note: Note) = noteDataSource.add(note)
+
+    suspend fun getNote(id: Long) = noteDataSource.get(id)
+
+    suspend fun getAllNotes() = noteDataSource.getAll()
+
+    suspend fun removeNote(note: Note) = noteDataSource.remove(note)
+}
